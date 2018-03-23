dda02a8474fd6fab10077532ecf27c045724eb87
Alexey Studenikin <avstdn@gmail.com>
2018-03-24 01:10:13 +0300

    Init

---
+++ init.rb
@@ -0,0 +1,28 @@
+require 'redmine'
+
+require_dependency 'redmine_telegram_notifications/notifier'
+
+Redmine::Plugin.register :redmine_telegram_notifications do
+  name 'Redmine Telegram Notifications'
+  author 'Alexey Studenikin'
+  url 'https://github.com/avstdn/redmine_telegram_notifications'
+  author_url 'https://github.com/avstdn/'
+  description 'Redmine Telegram Notifications'
+  version '1.0'
+
+  requires_redmine :version_or_higher => '2.0.0'
+
+  settings \
+  :default => {
+      'callback_url' => 'https://api.telegram.org/bot',
+      'channel' => nil,
+      'username' => 'redmine',
+      'display_watchers' => 'no',
+      'auto_mentions' => 'yes',
+      'new_include_description' => 1,
+      'updated_include_description' => 1,
+  },
+    :partial => 'view/settings'
+end
+
+
